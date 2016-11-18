f4a587a83557567356b7deb75f272bd5f0a61221
sqlfocus <lisongqing@126.com>
2016-11-18 14:44:32 +0800

    fix bug, 过多打印

--- set_client_addr.lua
+++ set_client_addr.lua
@@ -11,24 +11,27 @@ end
      1) 定制此线程绑定的IP地址
   ]]
 local thread_counter = 1             --[[用于线程计数，仿真线程ID]]
+local interface_name = "lo"
+local ip_addr_list = {eth2 = {}, eth3 = {}, lo = {}}
 function setup(thread)
    --[[获取客户端应该绑定的网卡名:
            如果目的地址为172.168.142.100, 则网卡应该为eth0
 	   如果目的地址为172.168.242.100, 则网卡应该为eth2
 	   本机测试，127.0.0.1, 则网卡应该为lo]]
-   local interface_name = "lo"
-   local sub_3rd_ip = wrk.host:sub(9, 9)
-   if sub_3rd_ip == "1" then
-       interface_name = "eth2"
-   elseif sub_3rd_ip == "2" then
-       interface_name = "eth3"
-   else
-       interface_name = "lo"
+   if thread_counter == 1 then
+       local sub_3rd_ip = wrk.host:sub(9, 9)
+       if sub_3rd_ip == "1" then
+           interface_name = "eth2"
+       elseif sub_3rd_ip == "2" then
+           interface_name = "eth3"
+       else
+           interface_name = "lo"
+       end
+       
+       print("interface: " .. interface_name)
    end
-   print("interface: " .. interface_name)
 
    --[[获取网卡的地址列表]]
-   local ip_addr_list = {eth2 = {}, eth3 = {}, lo = {}}
    if #ip_addr_list[interface_name] == 0 then
        local fp = io.popen("ip addr show " .. interface_name)
        for line in fp:lines() do
